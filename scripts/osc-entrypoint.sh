6b984e229844ee511028e57809208f38fb4e3e8f
Jonas Birmé <jonas.birme@eyevinn.se>
2025-12-18 20:58:41 +0100

    feat: enable in OSC

---
+++ scripts/osc-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# AIRFLOW_HOME=/opt/airflow by default in the base image
+
+if [ ! -z "$DATABASE_URL" ]; then
+  export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN="$DATABASE_URL"
+fi
+
+if [ ! -z "$OSC_HOSTNAME" ]; then
+  export AIRFLOW__WEBSERVER__BASE_URL="https://$OSC_HOSTNAME"
+  export AIRFLOW__CORE__EXECUTION_API_SERVER_URL="https://$OSC_HOSTNAME/execution/"
+fi
+
+if [ -z "$ADMIN_PASSWORD" ]; then
+  echo "ADMIN_PASSWORD is not set, using default 'admin'"
+  ADMIN_PASSWORD="admin"
+fi
+
+echo "{\"admin\": \"$ADMIN_PASSWORD\"}" > /opt/airflow/simple_auth_manager_passwords.json.generated
+
+airflow db migrate
+
+exec "/entrypoint" "$@"
